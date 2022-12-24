1dc87181bbfbe5688492b06eaf718847aae450dc
Sasha Osin <sashaosin364@gmail.com>
2022-12-24 23:50:37 +0700

    add plane

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,22 +1,28 @@
 import sys
-
+from settings import Setting
+from ship import Ship
 import pygame
 
 class AlienInvasion:
 
     def __init__(self):
         pygame.init()
+        self.settings = Setting()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_hight)
+        )
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
-            pygame.display.flip()
+                self.screen.fill(self.settings.bg_color)
+                self.ship.blitme()
+                pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Setting:
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_hight = 800
+        self.bg_color = (255,255,255)
